77bf649022ef3750d9745230a95645171b9cb406
dahamv <dahamviharana@gmail.com>
2020-10-09 08:47:33 +0000

    whats done upto filter-text box lesson

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { CustomersModule } from './customers/customers.module'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,10 +10,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    //This module provides directives.
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustomersModule
   ],
   providers: [],
+  //main.ts tells angular to bootstrap the app module.
   bootstrap: [AppComponent]
 })
 export class AppModule { }
